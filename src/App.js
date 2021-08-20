db78666ec769efe285637fecbb9183b74cedf8cb
robpwatkins <robpwatkins@gmail.com>
2021-08-20 16:56:36 -0500

    using hidden fields

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import { Widget } from '@typeform/embed-react';
 
 function App() {
   return (
-    <Widget id={process.env.REACT_APP_TYPEFORM_ID} style={{ width: "100%", height: "100vh" }} className="typeform" />
+    <Widget 
+      id={process.env.REACT_APP_TYPEFORM_ID}
+      style={{ width: "100%", height: "100vh" }}
+      hidden={{ 
+        portal_id: process.env.REACT_APP_PORTAL_ID,
+        form_guid: process.env.REACT_APP_FORM_GUID
+      }}
+    />
   );
 }
 
