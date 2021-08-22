df295a7f62b3f53dec327ae0db3ef07a70faaaa5
robpwatkins <robpwatkins@gmail.com>
2021-08-21 20:24:36 -0500

    added ip_address to hidden vars

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-
 import { Widget } from '@typeform/embed-react';
+import TagManager from 'react-gtm-module';
+import { useState, useEffect } from 'react';
+
+const tagManagerArgs = {
+  gtmId: 'GTM-PMVBB8T'
+}
+
+TagManager.initialize(tagManagerArgs)
 
 function App() {
+  const [ip, setIp] = useState();
+
+  useEffect(() => {
+    setIp(getGeolocation());
+  }, []);
+
+  const getGeolocation = async () => {
+    const geolocation = await (await fetch('https://geolocation-db.com/json/')).json();
+    return geolocation;
+  }
+
   return (
-    <Widget 
-      id={process.env.REACT_APP_TYPEFORM_ID}
-      style={{ width: "100%", height: "100vh" }}
-      hidden={{ 
-        portal_id: process.env.REACT_APP_PORTAL_ID,
-        form_guid: process.env.REACT_APP_FORM_GUID
-      }}
-    />
+    <>
+      <div>{ip}</div>
+      <Widget 
+        id={process.env.REACT_APP_TYPEFORM_ID}
+        style={{ width: "100%", height: "100vh" }}
+        hidden={{ 
+          portal_id: process.env.REACT_APP_PORTAL_ID,
+          form_guid: process.env.REACT_APP_FORM_GUID,
+          ip_address: ip
+        }}
+      />
+    </>
   );
 }
 
